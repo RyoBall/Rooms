504ec060c99ade76beb210b7160d9bf9edd87b80
brimoutain <1186521289@qq.com>
2025-04-23 19:30:44 +0800

    改了一点，加了拖拽跟随

---
+++ Assets/scripts/Obj/ShopObj.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ShopObj : MonoBehaviour
+{
+    public string itemID;         // 商品标识
+    public string itemName;       // 显示名称
+    public int price;             // 价格
+    public int maxStock = 1;      // 最大库存（0表示无限）
+    [TextArea] public string description; // 商品描述
+
+    virtual protected void OnMouseDown()
+    {
+        //货币是什么？（判断钱是不是大于价格
+        if (true)
+        {
+            if(maxStock > 1)
+            {
+                maxStock--;
+                //扣钱
+            }
+        }
+    }
+}
